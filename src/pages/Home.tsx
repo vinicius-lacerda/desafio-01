0ea182a4a65f620d5ec489624888e471fd60a882
Vinícius Lacerda <viniciuss.sync@gmail.com>
2021-08-25 14:23:42 -0400

    Resoluçao do desafio.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,32 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldState => [...oldState, data]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updateTasks = tasks.map(task => ({...task}));
+
+    const foundItem = updateTasks.find(item => item.id === id);
+
+    if(!foundItem)
+    return;
+
+    foundItem.done = !foundItem.done;
+    setTasks(updateTasks);
+    
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updateTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(updateTasks);
   }
 
   return (
